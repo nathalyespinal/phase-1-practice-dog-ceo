99332c2afd2165709dde4e0b85d3a50fb0f10815
NathalyEspinal <espinalnathaly@gmail.com>
2022-02-08 10:38:46 -0600

    commit tree ef3f597579eaca4ca486432fc9b81ae9ab12a017

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+
+//const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+//function getPhoto () { 
+    console.log('before fetch')
+console.log(fetch("https://dog.ceo/api/breeds/image/random/4") )
+  //.then(res => console.log (res))
+   
+//1.on page load, fetches the images using the url above ⬆
+
+//.then(function (response) {
+ //   return response.json();
+//})
+
+   //2.parses the response as `JSON`
+
+  
+  //.then(function (data) {
+ // })
+//}
+    // Use the data from the response to do DOM manipulation
+  //});
+
+//const imgContainer = document.getElementById("dog-image-container")
+//iterate through items to use them 
+//append a list 
+
+
+  //3.adds image elements to the DOM **for each** 🤔 image in the array
+
+
+
+
+
+
+
+
+
+
+
+
+
+//- on page load, fetches the images using the url above ⬆
